35e06ad7258f28bd4cf719ef90f5a030ccb3dd6c
peienting <1211311683@student.mmu.edu.my>
2024-07-21 15:02:16 +0800

    ring1

--- libs/RingProgressMesh.js
+++ libs/RingProgressMesh.js
@@ -6,11 +6,15 @@ void main() {
   vUv = uv;
   gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 }
-`
+`;
+
 const fshader = `
 #define PI2 6.28318530718
 
 uniform float uProgress;
+uniform vec4 uBgColor;
+uniform vec4 uArcColor;
+uniform float uArcThickness;
 
 varying vec2 vUv;
 
@@ -20,7 +24,7 @@ float circle(vec2 pt, vec2 center, float radius){
   return (len<radius) ? 1.0 : 0.0;
 }
 
-float arc(vec2 pt, vec2 center, float radius, float percent){
+float arc(vec2 pt, vec2 center, float radius, float percent, float thickness){
   float result = 0.0;
 
   vec2 d = pt - center;
@@ -32,7 +36,7 @@ float arc(vec2 pt, vec2 center, float radius, float percent){
     float arcAngle = PI2 * percent;
 
     float angle = mod( arcAngle - atan(d.y, d.x), PI2);
-    float edgeWidth = radius * 0.05;
+    float edgeWidth = radius * thickness;
     result = (angle<arcAngle) ? smoothstep(halfRadius, halfRadius + edgeWidth, len) - smoothstep(radius-edgeWidth, radius, len) : 0.0;
   }
 
@@ -41,44 +45,45 @@ float arc(vec2 pt, vec2 center, float radius, float percent){
 
 void main (void)
 {
-  vec4 bgColor = vec4(0.0, 0.0, 0.0, 1.0);
-  vec4 arcColor = vec4(1.0);
   vec2 center = vec2(0.5);
   vec4 color = vec4(0.0);
-  color += circle(vUv, center, 0.5) * bgColor;
-  color += arc(vUv, center, 0.4, uProgress) * arcColor;
+  color += circle(vUv, center, 0.5) * uBgColor;
+  color += arc(vUv, center, 0.4, uProgress, uArcThickness) * uArcColor;
   gl_FragColor = color; 
-}`
+}`;
 
-class RingProgressMesh extends Mesh{
-    constructor( scale = 1 ){
+class RingProgressMesh extends Mesh {
+    constructor(scale = 1, bgColor = [0, 0, 0, 1], arcColor = [1, 1, 1, 1], arcThickness = 0.05) {
         super();
-        
-        const uniforms = {
-          uProgress: { value: 0.0 },
-        }
 
-        this.material = new ShaderMaterial( {
-          uniforms: uniforms,
-          vertexShader: vshader,
-          fragmentShader: fshader,
-          alphaTest: 0.5,
-          transparent: true
-        } );
-        
+        const uniforms = {
+            uProgress: { value: 0.0 },
+            uBgColor: { value: new THREE.Vector4(...bgColor) },
+            uArcColor: { value: new THREE.Vector4(...arcColor) },
+            uArcThickness: { value: arcThickness },
+        };
+
+        this.material = new ShaderMaterial({
+            uniforms: uniforms,
+            vertexShader: vshader,
+            fragmentShader: fshader,
+            alphaTest: 0.5,
+            transparent: true
+        });
+
         this.geometry.dispose();
         this.geometry = new PlaneBufferGeometry();
-        this.scale.set( scale, scale, scale );
+        this.scale.set(scale, scale, scale);
         this.progress = 1;
     }
-    
-    set progress( value ){
-        if ( value<0 ) value = 0;
-        if ( value>1 ) value = 1;
+
+    set progress(value) {
+        if (value < 0) value = 0;
+        if (value > 1) value = 1;
         this.material.uniforms.uProgress.value = value;
     }
-    
-    get progress(){
+
+    get progress() {
         return this.material.uniforms.uProgress.value;
     }
 }
